6c1fa9f335fc9278932319098e0f3a1c437e798c
Aleksandr Lubenets <113028584+LubenetsAleksandr@users.noreply.github.com>
2022-09-29 01:09:58 +0400

    Update Theory.cs

--- Theory.cs
+++ Theory.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Diagnostics.CodeAnalysis;
 using System.Globalization;
 using System.Linq;
+using System.Net.Http.Headers;
 using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Text.Json;
@@ -18,7 +19,7 @@ namespace lab1s
             #region task 1_1
             Console.WriteLine("task 1_1");
             double r = 2;
-            double point1x=0, point1y=2;
+            double point1x = 0, point1y = 2;
             if (Math.Abs(point1x * point1x + point1y * point1y - r * r) <= 0.001)
             {
                 Console.WriteLine("first Point is in circle");
@@ -61,8 +62,8 @@ namespace lab1s
             Console.WriteLine("Enter point coordinates");
             int x = Convert.ToInt32(Console.ReadLine());
             int y = Convert.ToInt32(Console.ReadLine());
-            if (y>=-0 && y+Math.Abs(x)<=1)
-                {
+            if (y >= -0 && y + Math.Abs(x) <= 1)
+            {
                 Console.WriteLine("the point is in circle");
             }
             else
@@ -76,7 +77,7 @@ namespace lab1s
             double i;
             double s = 0;
             double n = Convert.ToInt32(Console.ReadLine());
-            if (n == 0) Console.WriteLine("Enter normal number");
+            if (n <= 0) Console.WriteLine("Enter normal number");
             else
             {
                 i = n;
@@ -84,24 +85,32 @@ namespace lab1s
                 while (i > 0)
                 {
                     x = Convert.ToInt32(Console.ReadLine());
-                    s += x;
-                    i = i - 1;
+                    if (x <= 0) Console.WriteLine("Enter normal number");
+                    else
+                    {
+                        s += x;
+                        i = i - 1;
+                    }
                 }
-                Console.WriteLine($"Boys average height - {s/n} ");
+                Console.WriteLine($"Boys average height - {s / n} ");
             }
             s = 0;
             Console.WriteLine("Enter amount of girls in class");
-            double g=Convert.ToInt32(Console.ReadLine());
-            if (g == 0) Console.WriteLine("Enter normal number");
+            double g = Convert.ToInt32(Console.ReadLine());
+            if (g <= 0) Console.WriteLine("Enter normal number");
             else
             {
                 i = g;
                 Console.WriteLine("Enter girls height");
-                while (i>0)
+                while (i > 0)
                 {
-                    x=Convert.ToInt32(Console.ReadLine());
-                    s += x;
-                    i = i - 1;
+                    x = Convert.ToInt32(Console.ReadLine());
+                    if (x <= 0) Console.WriteLine("enter normal number");
+                    else
+                    {
+                        s += x;
+                        i = i - 1;
+                    }
                 }
                 Console.WriteLine($"Girls average height - {s / g}");
             }
@@ -114,13 +123,13 @@ namespace lab1s
             n = double.Parse(Console.ReadLine());
             Console.WriteLine("write r of circle");
             r = 0;
-            r=double.Parse(Console.ReadLine());
+            r = double.Parse(Console.ReadLine());
             Console.WriteLine("write centre coordinates of circle");
             double a = double.Parse(Console.ReadLine());
             double b = double.Parse(Console.ReadLine());
             i = n;
             int kol = 0;
-            while (i>0)
+            while (i > 0)
             {
                 Console.WriteLine("Write point coordinates");
                 a1 = double.Parse(Console.ReadLine());
@@ -134,18 +143,25 @@ namespace lab1s
             Console.WriteLine($"amount of points in circle - {kol}");
             #endregion
             #region task 3_4
+            double swap;
             Console.WriteLine("Task 3_4");
             Console.WriteLine("Write abscissa of centre ring");
             double x0 = double.Parse(Console.ReadLine());
             Console.WriteLine("Write ordinate of centre ring");
             double y0 = double.Parse(Console.ReadLine());
             Console.WriteLine("Write r1");
-            double r1=double.Parse(Console.ReadLine());
+            double r1 = double.Parse(Console.ReadLine());
             Console.WriteLine("Write r2");
-            double r2=double.Parse(Console.ReadLine());
+            double r2 = double.Parse(Console.ReadLine());
+            if (r1 > r2)
+            {
+                swap = r2;
+                r2 = r1;
+                r1 = swap;
+            }
             n = 0;
             double flag = 0;
-            double  x3 = 0;
+            double x3 = 0;
             double y3 = 0;
             kol = 0;
             do
@@ -168,88 +184,93 @@ namespace lab1s
             double dva = 0;
             s = 0;
             kol = 0;
-            double neyt=0;
+            double neyt = 0;
             flag = 0;
+            double proverka = 0;
             do
             {
                 dva = 0;
-
+                proverka = 0;
                 Console.WriteLine("Write max of student, if you want to end programm write 0");
                 for (i = 1; i <= 4; i++)
                 {
                     n = double.Parse(Console.ReadLine());
                     if (n == 0) break;
                     if (n == 2) dva++;
-                    s += n;
+                    proverka += n;
                 }
                 if (n == 0) break;
                 kol += 1;
-                if (dva == 0) neyt++;
+                s += proverka;
+                if (dva != 0)
+                {
+                    neyt++;
+                    s = s - proverka;
+                }
             } while (flag != 1);
             if (kol == 0)
             { Console.WriteLine("No students"); }
             else
             {
-                 double gda = s / (kol * 4);
+                double gda = s / ((kol - neyt) * 4);
                 Console.WriteLine($"amount of losers - {neyt}, grade point avarage - {gda}");
             }
             #endregion
             #region task 3_12
-            string area;
+            int area;
             flag = 0;
             r = 0;
             s = 0;
             Console.WriteLine("Task 3_12");
             do
             {
-                Console.WriteLine("Which shape you want to calculate area of - square, triangle, circle. If you want to stop prog write - end");
-                area = Console.ReadLine();
-                if (area == "end") break;
-                if (area == "square")
+                Console.WriteLine("Which shape you want to calculate area of - square(write 1), triangle(write 2), circle (write 3). If you want to stop prog write - 0");
+                area = int.Parse(Console.ReadLine());
+                if (area == 0) break;
+                switch (area)
                 {
-                    Console.WriteLine("Write side length");
-                    r = double.Parse(Console.ReadLine());
-                    if (r == 0)
-                    {
-                        Console.WriteLine("Write correct length");
-                    }
-                    else
-                    {
-                        s = r * r;
-                        Console.WriteLine($"square area - {s}");
-                    }
+                    case 1:
+                        Console.WriteLine("Write side length");
+                        r = double.Parse(Console.ReadLine());
+                        if (r == 0)
+                        {
+                            Console.WriteLine("Write correct length");
+                        }
+                        else
+                        {
+                            s = r * r;
+                            Console.WriteLine($"square area - {s}");
+                        }
+                        break;
+                    case 2:
+
+                        Console.WriteLine("Write side of length");
+                        r = double.Parse(Console.ReadLine());
+                        if (r == 0)
+                        {
+                            Console.WriteLine("Write correct length");
+                        }
+                        else
+                        {
+                            s = Math.Sqrt(3) / 4 * r * r;
+                            Console.WriteLine("triangle area - {0:f4}", s);
+                        }
+                        break;
+                    case 3:
+
+                        Console.WriteLine("Write side of length");
+                        r = double.Parse(Console.ReadLine());
+                        if (r == 0)
+                        {
+                            Console.WriteLine("Write correct length");
+                        }
+                        else
+                        {
+                            s = Math.PI * r * r;
+                            Console.WriteLine("circle area - {0:f4}", s);
+                        }
+                        break;
                 }
-                if (area == "triangle")
-                {
-                    Console.WriteLine("Write side of length");
-                    r = double.Parse(Console.ReadLine());
-                    if (r == 0)
-                    {
-                        Console.WriteLine("Write correct length");
-                    }
-                    else
-                    {
-                        s = Math.Sqrt(3) / 4 * r * r;
-                        Console.WriteLine("triangle area - {0:f4}",s);
-                    }
-                }
-                if (area == "circle")
-                {
-                    Console.WriteLine("Write side of length");
-                    r = double.Parse(Console.ReadLine());
-                    if (r == 0)
-                    {
-                        Console.WriteLine("Write correct length");
-                    }
-                    else
-                    {
-                        s = Math.PI * r * r;
-                        Console.WriteLine("circle area - {0:f4}",s);
-                    }
-                }
-                        
-                    
-                
             } while (flag != 1);
             #endregion
             #region task 3_13
@@ -260,50 +281,52 @@ namespace lab1s
             Console.WriteLine("Task 3_13");
             do
             {
-                Console.WriteLine("Which shape you want to calculate area of rectangle, triangle, ring. If you want to end prog write - end");
-                area = Console.ReadLine();
-                if (area == "end")
+                Console.WriteLine("Which shape you want to calculate area of rectangle (Write 1), triangle (Write 2), ring (Write 3). If you want to end prog write - 0");
+                area = int.Parse(Console.ReadLine());
+                if (area == 0)
                 {
                     break;
                 }
-                if (area == "rectangle")
+                switch (area)
                 {
-                    Console.WriteLine("Write side lengths a and b");
-                    a = double.Parse(Console.ReadLine());
-                    b = double.Parse(Console.ReadLine());
-                    if (a == 0 || b == 0) Console.WriteLine("Write correct length");
-                    else
-                    {
-                        s = a * b;
-                        Console.WriteLine($"rectangle area - {s}");
-                    }
+                    case 1:
+                        
+                            Console.WriteLine("Write side lengths a and b");
+                            a = double.Parse(Console.ReadLine());
+                            b = double.Parse(Console.ReadLine());
+                            if (a == 0 || b == 0) Console.WriteLine("Write correct length");
+                            else
+                            {
+                                s = a * b;
+                                Console.WriteLine($"rectangle area - {s}");
+                            }
+                        break;
+                    case 2:
+                       
+                            Console.WriteLine("Write side lengths a and b");
+                            a = double.Parse(Console.ReadLine());
+                            b = double.Parse(Console.ReadLine());
+                            if (a == 0 || b == 0) Console.WriteLine("Write correct length");
+                            else
+                            {
+                                s = b / 4 * (Math.Sqrt(4 * a * a - b * b));
+                                Console.WriteLine("triangle area - {0:f4}", s);
+                            }
+                        break;
+                    case 3:
+                        
+                            Console.WriteLine("Write two radius a and b");
+                            a = double.Parse(Console.ReadLine());
+                            b = double.Parse(Console.ReadLine());
+                            if (a == 0 || b == 0) Console.WriteLine("write correct lenght");
+                            else
+                            {
+                                if (a > b) s = Math.PI * (a - b);
+                                else s = Math.PI * (b - a);
+                                Console.WriteLine("ring area - {0:f4}", s);
+                            }
+                        break;
                 }
-                if (area == "triangle")
-                {
-                    Console.WriteLine("Write side lengths a and b");
-                    a = double.Parse(Console.ReadLine());
-                    b = double.Parse(Console.ReadLine());
-                    if (a == 0 || b == 0) Console.WriteLine("Write correct length");
-                    else
-                    {
-                        s = b / 4 * (Math.Sqrt(4 * a * a - b * b));
-                        Console.WriteLine("triangle area - {0:f4}", s);
-                    }
-                }
-                if (area == "ring")
-                {
-                    Console.WriteLine("Write two radius a and b");
-                    a = double.Parse(Console.ReadLine());
-                    b = double.Parse(Console.ReadLine());
-                    if (a == 0 || b == 0) Console.WriteLine("write correct lenght");
-                    else
-                    {
-                        if (a > b) s = Math.PI * (a - b);
-                        else s = Math.PI * (b - a);
-                        Console.WriteLine("ring area - {0:f4}", s);
-                    }
-                }
-
             } while (flag != 1);
             #endregion
         }
