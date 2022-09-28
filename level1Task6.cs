a6ac56296c288556f3b627dc19e7f5ada8c9646f
Emil Gafarov <113677882+gafarovExe@users.noreply.github.com>
2022-09-28 23:38:23 +0300

    Update level1Task6.cs

--- level1Task6.cs
+++ level1Task6.cs
@@ -3,10 +3,12 @@ using System.Globalization;
 
 class Program
 {
-    static void Main()
+    static int Main()
     {
         double r = Convert.ToDouble(Console.ReadLine());
-        double s = Convert.ToDouble(Console.ReadLine()); 
+        double s = Convert.ToDouble(Console.ReadLine());
+        if (r < 0) {Console.WriteLine("r must not be less than zero"); return 0; }
+        if (s < 0) {Console.WriteLine("s must not be less than zero"); return 0; }
         double diametr = Math.Sqrt(r / Math.PI);
         double diagonal = Math.Sqrt(s * 2);
         if (diametr <= diagonal)
@@ -17,5 +19,6 @@ class Program
         {
             Console.WriteLine("No");
         }
+        return 0;
     }
 }
