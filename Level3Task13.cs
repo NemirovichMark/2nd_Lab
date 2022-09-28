1f0369095ae4ca798ad63ba885af46bed538f4a7
Emil Gafarov <113677882+gafarovExe@users.noreply.github.com>
2022-09-28 23:44:03 +0300

    Update Level3Task13.cs

--- Level3Task13.cs
+++ Level3Task13.cs
@@ -25,35 +25,36 @@ class HelloWorld
             if (strf == "END") break;
             else
             {
-                if (strf == "1")
+                switch (strf)
                 {
+                    case "1":
                     Console.WriteLine("Enter the first side of the rectangle:");
                     A = Convert.ToDouble(Console.ReadLine());
                     Console.WriteLine("Enter the second side of the rectangle:");
                     B = Convert.ToDouble(Console.ReadLine());
                     S = A * B;
-                }
-                if (strf == "2")
-                {
+                        break;
+                
+                case "2":
                     Console.WriteLine("Enter the radius of a smaller circle:");
                     A = Convert.ToDouble(Console.ReadLine());
                     Console.WriteLine("Enter the radius of a larger circle:");
                     B = Convert.ToDouble(Console.ReadLine());
                     S = B * B * Math.PI - A * A * Math.PI;
-                }
-                if (strf == "3")
-                {
+                        break;
+                case "3":
                     Console.WriteLine("Enter the first side of the isosceles triangle:");
                     A = Convert.ToDouble(Console.ReadLine());
                     Console.WriteLine("Enter the second side of the isosceles triangle:");
                     B = Convert.ToDouble(Console.ReadLine());
-                    S = A * Math.Sqrt(4 * B * B - A * A)/4;
+                    S = A * Math.Sqrt(4 * B * B - A * A) / 4;
+                break;
                 }
                 Console.WriteLine("The area of figure number " + t + ": " + S);
                 Console.WriteLine();
             }
             t++;
-        } 
+        }
         return 0;
     }
 }
