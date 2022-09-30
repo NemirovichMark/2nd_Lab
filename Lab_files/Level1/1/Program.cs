71467338e272f74bba8bd0ed6d403ef90b97fb64
Avenir <kadomcev2004@gmail.com>
2022-09-30 12:14:59 +0300

    L1N1

---
+++ Lab_files/Level1/1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace LaboratoryL1N1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double[] dots = {0,2, 1.5,0.7, 1,1, 3,0};
+            for (int i = 0; i < dots.Length; i = i + 2)
+            {
+                    double x = dots[i];
+                    double y = dots[i+1];
+                    if (((x*x) + (y*y) - 4) <= (1/1000))
+                    {
+                        Console.WriteLine($"({x} , {y}) on the circle");
+                    }
+                    else
+                    {
+                        Console.WriteLine($"({x} , {y}) outside the circle");
+                    }
+            }
+        }
+    }
+}
