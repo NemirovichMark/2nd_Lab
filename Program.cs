1b9e928b290626790268572fe57491b127c36c4a
IvashevichArtem <113028846+IvashevichArtem@users.noreply.github.com>
2022-10-12 23:15:08 +0300

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -106,6 +106,7 @@ namespace Project
                     break;
                 }
             }
+
             #endregion
             #region Task_2.5
             Console.WriteLine("2.5 Input: ");
@@ -117,14 +118,14 @@ namespace Project
             int count = 0;
             for (int i = 1; i <= 30; i++)
             {
-                if (!double.TryParse(Console.ReadLine(), out double x))
+                if (!double.TryParse(Console.ReadLine(), out double x1))
                 {
                     Console.WriteLine("2.5 Incorrect data, try again");
                     i--;
                 }
                 else
                 {
-                    if (x <= norm)
+                    if (x1 <= norm)
                     {
                         count++;
                     }
@@ -132,6 +133,246 @@ namespace Project
             }
             Console.WriteLine($"2.5 Answer: {count}");
             #endregion
+            #region Task_3.4
+
+            double r1, r2;
+            count = 0;
+
+            Console.WriteLine("3.4 Enter r1");
+            if (!double.TryParse(Console.ReadLine(), out r1))
+            {
+                Console.WriteLine("3.4 It`s not a number.");
+                return;
+            }
+
+            Console.WriteLine("Enter r2");
+            if (!double.TryParse(Console.ReadLine(), out r2))
+            {
+                Console.WriteLine("3.4 It`s not a number.");
+                return;
+            }
+
+            Console.WriteLine("3.4 Еnter a coordinate with a space, example: 10 13");
+            string s;
+            double x, y;
+
+            while (true)
+            {
+                s = Console.ReadLine();
+
+                if (s == "")
+                {
+                    break;
+                }
+
+                string[] nums = s.Split();
+
+                if (nums.Length != 2)
+                {
+                    Console.WriteLine("3.4 Wrong input. Let`s try again.");
+                    continue;
+                }
+
+                if (!double.TryParse(nums[0], out x))
+                {
+                    Console.WriteLine("3.4 x is not a number. Let`s try again.");
+                    continue;
+                }
+                if (!double.TryParse(nums[1], out y))
+                {
+                    Console.WriteLine("3.4 y is not a number. Let`s try again.");
+                    continue;
+                }
+
+                if ((r1 * r1 <= x * x + y * y) && (r2 * r2 >= x * x + y * y))
+                {
+                    count++;
+                }
+            }
+
+            Console.WriteLine($"Task 3.4 answer {count}");
+
+            #endregion
+            #region Task_3.11
+
+            Console.WriteLine("3.11 Еnter all 4 marks with a space between, example: 4 5 3 3");
+
+
+            string[] marks;
+
+            int failingStudents = 0;
+            int sumOfMarks = 0;
+            int numberOfStudents = 0;
+
+            while (true)
+            {
+                s = Console.ReadLine();
+                if (s == "")
+                {
+                    break;
+                }
+
+                marks = s.Split();
+                if (marks.Length != 4)
+                {
+                    Console.WriteLine("3.11 Wrong input. Let`s try again.");
+                    continue;
+                }
+
+                int[] intMarks = { 0, 0, 0, 0 };
+                bool isFailng = false;
+                bool wrongInput = false;
+
+                for (int i = 0; i < 4; i++)
+                {
+                    if (!int.TryParse(marks[i], out intMarks[i]))
+                    {
+                        wrongInput = true;
+                    }
+                }
+
+                if (wrongInput)
+                {
+                    Console.WriteLine("3.11 One of the marks isn`t integer. Let`s try again.");
+                    continue;
+                }
+
+                foreach (int mark in intMarks)
+                {
+                    if (mark <= 2)
+                    {
+                        isFailng = true;
+                    }
+                    sumOfMarks += mark;
+                }
+
+                if (isFailng)
+                {
+                    failingStudents += 1;
+                }
+
+                numberOfStudents += 1;
+            }
+
+            Console.WriteLine($" 3.11 Answer: Failing students: {failingStudents} Average mark: {sumOfMarks / 4.0 / numberOfStudents}");
+            #endregion
+            #region Task_3.12
+
+            double r;
+            string cmd;
+
+            while (true)
+            {
+                Console.WriteLine("3.12 Enter r: ");
+
+                s = Console.ReadLine();
+                if (s == "")
+                {
+                    break;
+                }
+
+                if (!double.TryParse(s, out r))
+                {
+                    Console.WriteLine("It`s not a number. Let`s try again.");
+                    continue;
+                }
+
+                if (r < 0)
+                {
+                    Console.WriteLine("Wrong input (r < 0). Let`s try again.");
+                    continue;
+                }
+
+                Console.WriteLine("Number commands\n1. rectangle area\n2. ring area\n3. triange area\nEnter command");
+                cmd = Console.ReadLine();
+
+                switch (cmd)
+                {
+                    case "1":
+                        Console.WriteLine($"Area of the square: {r * r}");
+                        break;
+                    case "2":
+                        Console.WriteLine($"Area of the cirle: {r * r * Math.PI}");
+                        break;
+                    case "3":
+                        Console.WriteLine($"Area of the triangle: {(Math.Sqrt(3) / 4) * r * r}");
+                        break;
+                    default:
+                        Console.WriteLine("Unknown command");
+                        break;
+                }
+            }
+
+
+            #endregion
+            #region Task_3.13
+            Console.WriteLine("3.12 Еnter a and b with a space between, example: 10 13");
+
+
+            double a1, b1;
+
+
+            while (true)
+            {
+                Console.WriteLine("3.12 Enter a and b");
+                s = Console.ReadLine();
+
+                if (s == "")
+                {
+                    break;
+                }
+
+                string[] nums = s.Split();
+
+                if (nums.Length != 2)
+                {
+                    Console.WriteLine("Wrong input. Let`s try again.");
+                    continue;
+                }
+
+                if (!double.TryParse(nums[0], out a1))
+                {
+                    Console.WriteLine("a is not a number. Let`s try again.");
+                    continue;
+                }
+                if (!double.TryParse(nums[1], out b1))
+                {
+                    Console.WriteLine("b is not a number. Let`s try again.");
+                    continue;
+                }
+
+                if (a1 < 0 || b1 < 0)
+                {
+                    Console.WriteLine("Wrong input: a < 0 or b < 0. Let`s try again.");
+                    continue;
+                }
+
+                Console.WriteLine("Number commands\n1. square area\n2. circle area\n3. triange area\nEnter command");
+                cmd = Console.ReadLine();
+
+                switch (cmd)
+                {
+                    case "1":
+                        Console.WriteLine($"Area of the square: {a1 * b1}");
+                        break;
+                    case "2":
+                        Console.WriteLine($"Area of the cirle: {Math.Abs(a1 * a1 * Math.PI - b1 * b1 * Math.PI)}");
+                        break;
+                    case "3":
+                        if (Math.Max(a1, b1) > (a1 + b1 + b1) - Math.Max(a1, b1))
+                        {
+                            Console.WriteLine("There is no triangle with such sides");
+                            break;
+                        }
+                        Console.WriteLine($"Area of the triangle: {Math.Sqrt(b1 * b1 - Math.Pow((a1 / 2.0), 2)) * a1 / 2.0}");
+                        break;
+                    default:
+                        Console.WriteLine("Unknown command");
+                        break;
+                }
+            }
+            #endregion
         }
-    }    
+
+    }
 }
