837640391f242a668dc380c88112f639c9f6a145
Krauze Vladislav <113583763+kaknokote@users.noreply.github.com>
2022-11-01 18:53:49 +0300

    Исправление ошибок
    да

--- Program.cs
+++ Program.cs
@@ -1,175 +1,244 @@
 ﻿using System;
 
-namespace _1st_Lesson
+namespace Lab2
 {
-    class Program
+    class Theory
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("1/3");
+            #region 1/3
+            try
+            {
+                double a, b;
+                a = double.Parse(Console.ReadLine());
+                b = double.Parse(Console.ReadLine());
+                if (a > 0) 
+                {
+                    Console.WriteLine("c = " + Math.Max(a,b));
+                }
+                else 
+                {
+                    Console.WriteLine("c = " + Math.Min(a,b));
+                }
+            }
+            catch 
+            {
+                Console.WriteLine("Программа поддерживает только числа");
+            }
+            #endregion
+            Console.WriteLine("1/4");
+            #region 1/4
+            try 
+            {
+                double a, b, c;
+                a = double.Parse(Console.ReadLine());
+                b = double.Parse(Console.ReadLine());
+                c = double.Parse(Console.ReadLine());
+                Console.WriteLine("z = " + Math.Max(Math.Min(a, b), c));
+            }
+            catch
+            {
+                Console.WriteLine("Программа поддерживает только числа");
+            }
+
+            #endregion
+            Console.WriteLine("1/2");
+            #region 1/2
+            try 
+            {
+                int n = 3;
+                double r, mid;
+                double s = 0;
+                for (int i = 1; i <= n; i++)
+                {
+                    Console.WriteLine("Введите рост ученика");
+                    r = double.Parse(Console.ReadLine());
+                    while (r <= 0) 
+                    {
+                        Console.WriteLine("Рост должен быть > 0");
+                        r = double.Parse(Console.ReadLine());
+                    }
+                    s += r;
+                }
+                mid = s / n;
+                Console.WriteLine("Средний рост всех учеников =" + mid);
+            }
+            catch 
+            {
+                Console.WriteLine("Программа поддерживает только числа");
+            }
+            #endregion
+            Console.WriteLine("5/2");
+            #region 5/2
             
-        #region 1/3
-
-        double a, b;
-        a = Convert.ToDouble(Console.ReadLine());
-        b = Convert.ToDouble(Console.ReadLine());
-
-        if (a > 0) 
-        {
-            Console.WriteLine(Math.Max(a,b));
-        }
-        else 
-        {
-            Console.WriteLine(Math.Min(a,b));
-        }
-
-        #endregion 1/3
-        #region 4/1
-        double x;
-        a = Convert.ToDouble(Console.ReadLine());
-        b = Convert.ToDouble(Console.ReadLine());
-        double c = Convert.ToDouble(Console.ReadLine());
-        x = Math.Min(a,b);
-        Console.WriteLine(Math.Max(x,c));
-
-        #endregion 4/1
-
-        #region 1/2
-        int n = 3;
-        double sr, r, s = 0;
-        for (int i = 1; i <= n; i++)
-        {
-            Console.WriteLine("Введите рост ученика");
-            r = Convert.ToDouble(Console.ReadLine());
-            s += r;
-        }
-        sr = s / n;
-        Console.WriteLine("Средний рост = " + sr);
-        #endregion 1/2
-
-        #region 5/2 
-
-        n = 30;
-        c = 0;
-        double norma;
-        Console.WriteLine("Введите норматив");
-        norma = Convert.ToDouble(Console.ReadLine());
-        for (int i = 1; i <= n; i++) 
-        {
-            Console.WriteLine("Введите результат участника");
-            if (Convert.ToDouble(Console.ReadLine()) >= norma)
+            try 
             {
-                Console.WriteLine("Норматив сдан");
-                c++;
+                double  c = 0;
+                double n = 30;
+                double norm, res;
+                norm = double.Parse(Console.ReadLine());
+                for (int i = 1; i <= n; i++)
+                {
+                    Console.WriteLine("Введите результат спортсмена " + i);
+                    res = double.Parse(Console.ReadLine());
+                    while (res <= 0)
+                    {
+                        Console.WriteLine("Результат не может быть отрицательным");
+                        res = double.Parse(Console.ReadLine());
+                    }
+                    if (res < norm)
+                    {
+                        c += 1;
+                    }
+                }
+                Console.WriteLine("Норматив выполнили " + c + "спортсменов");
             }
-            else 
+            catch
             {
-                Console.WriteLine("Норматив не сдан");
+                Console.WriteLine("Программа поддерживает только числа");
             }
-        }
-        Console.WriteLine($"{c}");
-        #endregion 5/2
-        
-        #region 4/3
-
-        s = 0;
-        Console.WriteLine("Введите количество точек");
-        double k = Convert.ToDouble(Console.ReadLine());
-        Console.WriteLine("Введите координаты центра окружности");
-        a = Convert.ToDouble(Console.ReadLine());
-        b = Convert.ToDouble(Console.ReadLine());
-        Console.WriteLine("Введите радиус внутреннего кольца");
-            r = Convert.ToDouble(Console.ReadLine());
+            #endregion
+            Console.WriteLine("4/3");
+            #region 4/3 
+            try 
+            {
+            double s = 0;
+            Console.WriteLine("Введите количество точек");
+            double n = double.Parse(Console.ReadLine());
+            Console.WriteLine("Введите координаты центра окружности по Х и У соответсвенно");
+            double a = double.Parse(Console.ReadLine());
+            double b = double.Parse(Console.ReadLine());
+            Console.WriteLine("Введите радиус внутреннего кольца");
+            double r = double.Parse(Console.ReadLine());
             Console.WriteLine("Введите радиус внешнего кольца");
-            double r2 = Convert.ToDouble(Console.ReadLine());
-            for (int i = 1; i <= k; i++)
-            {
-                Console.WriteLine("Введите координаты по оси x");
-                x = Convert.ToDouble(Console.ReadLine());
-                Console.WriteLine("Введите координаты по оси y");
-                double y = Convert.ToDouble(Console.ReadLine());
-
-                double q = Math.Sqrt((Math.Pow((x - a), 2) + Math.Pow((y - b), 2)));
-                if (q >= r && q <= r2)
-                {
-                    s++;
-                    Console.WriteLine("Точка принадлежит кольцу");
-                }
-            }
-            Console.WriteLine($"{s}");
-
-        #endregion 4/3
-
-        #region 11/3
-
-        Console.WriteLine("Введите кол-во студентов");
-        double m = 0;
-        c = 0;
-        k = Convert.ToDouble(Console.ReadLine());
-        Console.WriteLine("Введите кол-во экзаменов");
-        double e = Convert.ToDouble(Console.ReadLine());
-        for (int i = 1; i < k; i++)
-        {
-            bool flag = true;
-            for (int j = 1; j < e; j++)
-            {
-                Console.WriteLine("Введите оценку за экзамен");
-                double z = Convert.ToDouble(Console.ReadLine());
-                m += z;
-                if (z < 3)
-                {
-                    flag = false;
-                }
-            if (flag == false)
-                {
-                    c++;
-                }
-
-            }
-        }
-        Console.WriteLine("Средний балл = " + m / (k*e));
-        Console.WriteLine("Кол-во незачетов = " + c);
-        
-        #endregion 11/3
-
-        #region 12/3
-            Console.WriteLine("Введите количество значений");
-            n = int.Parse(Console.ReadLine());
+            double R = double.Parse(Console.ReadLine());
             for (int i = 1; i <= n; i++)
             {
-                Console.WriteLine("Введите значение r");
-                r = Convert.ToDouble(Console.ReadLine());
-                Console.WriteLine("Какая еще фигура?");
-                string figure = Console.ReadLine();
-                if (figure == "Квадрат")
+                Console.WriteLine("Введите координаты по оси x");
+                double x = double.Parse(Console.ReadLine());
+                Console.WriteLine("Введите координаты по оси y");
+                double y = double.Parse(Console.ReadLine());
+
+                double q = Math.Sqrt((Math.Pow((x - a), 2) + Math.Pow((y - b), 2)));
+                if (q >= r && q <= R)
                 {
-                    Console.WriteLine("Площадь квадрата =" + r * r);
-                }
-                if (figure == "Круг")
-                {
-                    Console.WriteLine("Площадь круга =" + Math.PI * r * r);
-                }
-                if (figure == "Треугольник")
-                {
-                    Console.WriteLine("Площадь треугольника =" + r * r * Math.Sqrt(3) / 4);
+                    s++;
                 }
             }
+            Console.WriteLine(s + " точек лежит в кольце");
+            }
+            catch
+            {
+            Console.WriteLine("Программа поддерживает только числа");
+            }
+            #endregion 
+            Console.WriteLine("11/3");
+            #region 11/3
+            try 
+            {
+                
+                Console.WriteLine("Введите кол-во студентов, сдающих экзамен");
+                double n = double.Parse(Console.ReadLine());
+                Console.WriteLine("Введите кол-во экзаменов");
+                double ex = double.Parse(Console.ReadLine());
+                double o, ne = 0, s = 0, c = 0, sum = 0, sumo = 0;
+                for (int i = 1; i <= n; i++)
+                {
+                    Console.WriteLine("Студент " + i);
+                    for (int j = 1; j <= ex; j++)
+                    {
+                        Console.WriteLine("Введите оценку за экзамен " + j);
+                        o = double.Parse(Console.ReadLine()); 
+                        if (o > 3)
+                        {
+                            sum += o;
+                            c += 1;
+                        }
+                    }
+                    if (c == ex)
+                    {
+                        s += 1;
+                        sumo += sum;
+                    }
+                    else 
+                    {
+                        ne += 1;
+                    }
+                    sum = 0;
+                    c = 0;
+                }
+                Console.WriteLine("Средний балл студентов = " + (sumo / ex) / n);
+                Console.WriteLine("Экзамен не сдало " + ne + " студентов");
+            }
+            catch
+            {
+                Console.WriteLine("Программа поддерживает только числа");
+            }
+            #endregion
+            Console.WriteLine("12/3");
+            #region 12/3
+            try 
+            {
+                Console.WriteLine("Введите количество значений");
+                double n = int.Parse(Console.ReadLine());
+                for (int i = 1; i <= n; i++)
+                {
+                    Console.WriteLine("Введите значение r");
+                    double r = Convert.ToDouble(Console.ReadLine());
+                    while (r <= 0)
+                    {
+                        Console.WriteLine("r <= 0? Вероятно, вы не правы");
+                        r = Convert.ToDouble(Console.ReadLine());
+                    }
 
-            #endregion 12/3
+                    Console.WriteLine("Какая еще фигура?");
+                    string figure = Console.ReadLine();
+                    if (figure == "Квадрат")
+                    {   
+                        Console.WriteLine("Площадь квадрата =" + r * r);
+                    }
+                    if (figure == "Круг")
+                    {
+                        Console.WriteLine("Площадь круга =" + Math.PI * r * r);
+                    }
+                    if (figure == "Треугольник")
+                    {
+                        Console.WriteLine("Площадь треугольника =" + r * r * Math.Sqrt(3) / 4);
+                    }
+                }
+            }
+            catch
+            {
+                Console.WriteLine("Программа поддерживает только числа");
+            }
+            #endregion 
+            Console.WriteLine("13/3");
+            #region 13/3
 
-            #region 12/4
+            try 
+            {
             Console.WriteLine("Введите количество пар значений");
-            n = int.Parse(Console.ReadLine());
+            double n = int.Parse(Console.ReadLine());
             for (int i = 1; i <= n; i++)
             {
                 Console.WriteLine("Введите значение для А");
-                a = Convert.ToDouble(Console.ReadLine());
+                double a = Convert.ToDouble(Console.ReadLine());
+                while (a <= 0)
+                {
+                    Console.WriteLine("a > 0 нужно");
+                }
                 Console.WriteLine("Введите значение для B");
-                b = Convert.ToDouble(Console.ReadLine());
+                double b = Convert.ToDouble(Console.ReadLine());
+                while (b < 0)
+                {
+                    Console.WriteLine("b > 0 нужно");
+                }
                 Console.WriteLine("Какая еще фигура?");
                 string figure = Console.ReadLine();
                 if (figure == "Прямоугольник")
                 {
-                    Console.WriteLine("Площадь прямоугольника =" + a * b);
+                    Console.WriteLine("Площадь прямоугольника = " + a * b);
                 }
                 if (figure == "Кольцо")
                 {
@@ -177,11 +246,23 @@ namespace _1st_Lesson
                 }
                 if (figure == "Треугольник")
                 {
-                    Console.WriteLine("Площадь треугольника = " + (Math.Sqrt(4 * a * a - b * b) * b / 4));
+                    try 
+                    {
+                        Console.WriteLine("Площадь треугольника = " + (Math.Sqrt(4 * a * a - b * b) * b / 4));
+                    }
+                    catch
+                    {
+                        Console.WriteLine("Треугольникa с такими сторонами не существует");
+                    }
                 }
             }
+            }
+            catch
+            {
+                Console.WriteLine("Программа поддерживает только числа");
+            }
 
-            #endregion 12/4
-        }
+            #endregion
+        } 
     }
 }
