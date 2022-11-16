67ef764365512a53cf3d9455c4a426bfda512523
EmilTsvetanov0 <113028536+EmilTsvetanov0@users.noreply.github.com>
2022-11-16 16:17:59 +0300

    Update Level3_Task13.cs

--- Level3_Task13.cs
+++ Level3_Task13.cs
@@ -14,17 +14,21 @@ namespace Sololearn
             int n;
             double A=0,B=0,S=0,p=0;
             string choice;
+            bool f=false;
             Console.Write("Enter n: ");
             if(!int.TryParse(Console.ReadLine(),out n))
                 return;
             for(int i=0; i<n; i++){
+                f=false;
                 Console.Write("Enter A: ");
                 if(!double.TryParse(Console.ReadLine(),out A))
                     return;
                 Console.Write("Enter B: ");
                 if(!double.TryParse(Console.ReadLine(),out B))
                     return;
-                
+                A=abs(A);
+                B=abs(B);
+                f=(A+B<=A)||(A+A<=B);
                 Console.Write("What do you want to do?\n-Find S of rectangle(1)\n-Find S of ring(2)\n-Find S of triangle(3)\nYour choice: ");
                 choice=Console.ReadLine();
                 switch(choice){
@@ -35,8 +39,14 @@ namespace Sololearn
                         S=abs(Math.PI*A*A-Math.PI*B*B);
                         break;
                     case "3":
-                        p=(A+A+B)/2;
-                        S=Math.Sqrt(p*(p-A)*(p-A)*(p-B));
+                        if(f){
+                            Console.WriteLine("There is no triangle with these sides");
+                            S=0;
+                        }
+                        else{
+                            p=(A+A+B)/2;
+                            S=Math.Sqrt(p*(p-A)*(p-A)*(p-B));
+                        }
                         break;
                 }
                 Console.WriteLine($"The S equals: {S}");
